f1ed0cf50e6e8847f4edaaf587b2a6d4f9c5d7c2
arkherim <victorlermen.vl@gmail.com>
2026-07-01 23:52:49 -0300

    fix front

--- app/frontend/nitro.config.ts
+++ app/frontend/nitro.config.ts
@@ -2,7 +2,18 @@ import { defineNitroConfig } from "nitro/config";
 
 export default defineNitroConfig({
   preset: "vercel",
+  vercel: {
+    functions: {
+      runtime: "nodejs22.x",
+    },
+  },
   externals: {
-    inline: ["tslib"],
+    inline: [
+      "tslib",
+      "react-remove-scroll",
+      "react-remove-scroll-bar",
+      "react-style-singleton",
+      "use-callback-ref",
+    ],
   },
 });
